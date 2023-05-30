bb308562320c5cbfc2e7bb66a799185ba1cfa90e
Brad Wheeler <ordersponge@gmail.com>
2023-05-30 11:37:38 -0700

    Revert "initialize course dataset"
    This reverts commit d5edfb4e4a0d9566f0c94e7f477d950392b8bbd3.

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,5 +1,5 @@
 # Run an initial setup script for the MariaDB database
 if [ -f .devcontainer/setup-mariadb.sql ]; then
   sleep 10
-  mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/h+sports_01_01_initial_dataset.sql
+  mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql
 fi
