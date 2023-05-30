39b566f9db2e9547e673ca09dbb3dabf0231bfb7
Brad Wheeler <ordersponge@gmail.com>
2023-05-30 16:56:38 -0700

    revisions

--- Challenges/01_08c.sql
+++ Challenges/01_08c.sql
@@ -2,7 +2,7 @@ UPDATE transactions SET Price = Replace(Price, '$', '');
 UPDATE transactions SET Discount = Replace(Discount, '%', '');
 UPDATE transactions SET Discount = Discount / 100;
 
-INSERT INTO customer 
+INSERT INTO customer
 SELECT DISTINCT CustomerID, CustName, CustState, CustomerType FROM transactions;
 
 INSERT INTO employee 
